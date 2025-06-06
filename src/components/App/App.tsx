c001b6980a9d0303cf5e553c1115191c31ad0586
unknown <ab261099tro@H-ab261099tro.ws.a-bank.com.ua>
2025-06-06 04:15:49 +0300

    finish

---
+++ src/components/App/App.tsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import styles from "./App.module.css"
+import toast, { Toaster } from 'react-hot-toast';
+import { fetchMovies } from '../../services/movieService';
+import { type TMDBResponse, type Movie } from '../../types/movie';
+import  SearchBar  from '../SearchBar/SearchBar';
+import  MovieGrid  from '../MovieGrid/MovieGrid';
+import  Loader  from '../Loader/Loader';
+import  ErrorMessage  from '../ErrorMessage/ErrorMessage';
+import  MovieModal  from '../MovieModal/MovieModal';
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
+import ReactPaginate from 'react-paginate';
+
+export default function App() {
+  const [query, setQuery] = useState(``);
+  const [page, setPage] = useState(1);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+
+ const {data, isLoading, isError} = useQuery<TMDBResponse<Movie>>({
+  queryKey: [`movies`, query, page],
+  queryFn: () => fetchMovies(query,page),
+  enabled: query !== "",
+  placeholderData: keepPreviousData,
+ })
+
+ const handleSearch = (newQuery: string) => {
+  setQuery(newQuery);
+  setPage(1);
+ }
+
+
+
+
+
+
+  return (
+    <div className={styles.app}>
+      <SearchBar onSubmit={handleSearch} />
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
+      {!isLoading && !isError && data?.results.length === 0 && (
+        toast.error("No movies found for your request.")
+      )}
+      {!isLoading && !isError && data && data.results.length > 0 && (<> 
+      {data.total_pages > 1 && (
+            <ReactPaginate
+              pageCount={data.total_pages}
+              pageRangeDisplayed={5}
+              marginPagesDisplayed={1}
+              onPageChange={({ selected }) => setPage(selected + 1)}
+              forcePage={page - 1}
+              containerClassName={styles.pagination}
+              activeClassName={styles.active}
+              nextLabel="→"
+              previousLabel="←"
+            />
+          )}
+      <MovieGrid movies={data.results} onSelect={setSelectedMovie} />
+      </>)}
+      {selectedMovie && (<MovieModal movie={selectedMovie} onClose={() => setSelectedMovie(null)} />)}
+      <Toaster />
+    </div>
+  );
+}
---
+++ src/services/movieService.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { type Movie } from '../types/movie';
+
+interface TMDBResponse<T> {
+    page: number
+    results: T[]
+    total_results: number
+    total_pages: number
+
+}
+
+const BASE_URL = "https://api.themoviedb.org/3/search/movie";
+
+export async function fetchMovies(query:string, page = 1): Promise<TMDBResponse<Movie>> {
+    const response = await axios.get<TMDBResponse<Movie>>(BASE_URL, {
+        params: {query, page},
+        headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
+        },
+    });
+    return response.data;
+}
